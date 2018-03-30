8eb7de7deb26a69e7cc432aacd90d0f057ede072
Guy Pender <guy.pender@me.com>
2018-03-30 22:10:07 +0100

    Added list of drives found

--- public/app.js
+++ public/app.js
@@ -1,16 +1,46 @@
 var app = angular.module('ac30-pi',[]);
 
-app.controller('controller',['$scope',function($scope){
-    var socket = io();
+app.controller('controller',['$scope','socket','$window',function($scope,socket,window){
     var vm = this;
     vm.guy = 'hello';
+    vm.drives=[];
+    window.onload= function() { 
+        vm.scanDrives();
+    };
 
     socket.on('driveFound',function(drive){
         console.log(drive);
-        vm.guy = drive;
+        vm.drives.push(JSON.parse(drive));
     });
 
     vm.scanDrives = function(){
+        vm.drives=[];
         socket.emit('scanDrives');
     };
-}])
+}]);
+
+app.factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {  
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        })
+      }
+    };
+});
+
+
