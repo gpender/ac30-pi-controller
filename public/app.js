fca66a7fa07146f980d32ca9b2bed0c3c8bd71d7
Guy Pender <guy.pender@me.com>
2018-03-30 22:29:18 +0100

    Move socket into new factory and new file, sorted drives list

--- public/app.js
+++ public/app.js
@@ -19,28 +19,4 @@ app.controller('controller',['$scope','socket','$window',function($scope,socket,
     };
 }]);
 
-app.factory('socket', function ($rootScope) {
-    var socket = io.connect();
-    return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {  
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            if (callback) {
-              callback.apply(socket, args);
-            }
-          });
-        })
-      }
-    };
-});
-
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-var socket = io();
+//var socket = io();
 
 
 //var scanButton = jQuery('#scan-drives');
